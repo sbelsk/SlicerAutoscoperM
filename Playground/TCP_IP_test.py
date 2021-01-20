17dbd8b23c833d1a65dcc50f4328e855638cdec4
Bardiya Ak <bardiya.akhbari@gmail.com>
2021-01-20 13:33:03 -0500

    added QTcpSocket connection to the playground. Autoscoper still is not able to read the path.

--- Playground/TCP_IP_test.py
+++ Playground/TCP_IP_test.py
@@ -1,15 +1,32 @@
-### CONNECTION (firs implementation using socket)
-import socket
-from struct import pack
+# ### CONNECTION (firs implementation using socket)
+# import socket
+# from struct import pack
 
-client  = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-print('socket instantiated')
+# client  = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# print('socket instantiated')
+# HOST = 'localhost'
+# PORT = 30007
+# client.connect((HOST, PORT))
+# print('socket connected')
+
+# conn_type = 1
+# pp = b"C:\\Dev\\autoscoper-git\\build\\install\\bin\\Release\\sample_data\\wrist.cfg"
+# data = pack('i 66s', conn_type, pp)
+# client.sendall(data)
+
+
+### CONNECTION: QTcpSocket
+import qt
 HOST = 'localhost'
 PORT = 30007
-client.connect((HOST, PORT))
-print('socket connected')
+client = qt.QTcpSocket()
+client.connectToHost(HOST, PORT)
+client.waitForConnected()
 
-conn_type = 1
-pp = b"C:\Dev\autoscoper-git\build\install\bin\Release\sample_data\wrist.cfg"
-data = pack('i 66s', conn_type, pp)
-client.sendall(data)
+
+conn_type = b"1"
+pp = b"C:\\Dev\\autoscoper-git\\build\\install\\bin\\Release\\sample_data\\wrist.cfg"
+data = conn_type + pp
+client.write(data)
+client.flush()
+
