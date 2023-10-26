ba891d3d3e137ee2c57e1f744e42a3b601cc2ab2
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-10-26 09:51:30 -0400

    ENH: Update Autoscoper with various enhancements and fixes
    List of Autoscoper changes:
    
    $ git shortlog 75eb9ad5a..73eb359dc --no-merges
    Anthony Lombardi (14):
          STYLE: Refactor mayacam readers
          DOC: Fix incorrect statements in the camera calibration file specs
          ENH: Add VTKCam Reader
          ENH: Add Schema for vtkCamera JSON file
          ENH: Refactor VTK Cam loader for JSON files
          BUG: Fix incorrect variable name in VTKCamera loader
          DOC: Tweak naming convension for camera in cfg docs
          DOC: Fix typo in cfg file specs
          DOC: Update volume subdir from VolumeImages to Volumes
          PERF: Switch back to original viewport conversion (#212)
          ENH: Add option to enable/disable a single DRR
          DOC: Describe Tracker optimize() and trackFrame() functions
          BUG: Ensure getRandom() returns values within range (#219)
          COMP: Fix CUDA RayCaster not having setVisible member
    
    Jean-Christophe Fillion-Robin (7):
          COMP: Fix Camera.cpp build error including "algorithm" required for std::remove
          COMP: Fix -Wreturn-local-addr in Camera class
          COMP: Fix constness of "lookAt" function
          STYLE: Improve name consistency renaming "lookAt" to "calculateLookAtMatrix"
          COMP: Remove unused and undefined Camera::calculateRotationMatrix function
          COMP: Fix standalone build updating JsonCpp integration
          STYLE: Consistently indent Camera.cpp

--- SuperBuild/External_Autoscoper.cmake
+++ SuperBuild/External_Autoscoper.cmake
@@ -31,7 +31,7 @@ if(NOT DEFINED ${proj}_DIR AND NOT ${SUPERBUILD_TOPLEVEL_PROJECT}_USE_SYSTEM_${p
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "75eb9ad5ac1ab490b1bb1f8e0f97bd60d86f8a99"
+    "73eb359dcd70878c4b4c28471fd2684c08bd3014"
     QUIET
   )
 
