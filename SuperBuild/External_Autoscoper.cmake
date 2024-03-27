e1a65df9bc39efb4fa4b3efc0fdcf82521a682e3
Anthony Lombardi <30351234+NicerNewerCar@users.noreply.github.com>
2024-03-27 13:59:28 -0400

    BUG: Update Autoscoper to fix edge cases in Slerp (#90)
    Regression initially introduced in BrownBiomechanics/Autoscoper@6b7ad8e.
    Slerp interpolation fails in some cases when t == 0 or t == 1
    
    ```
    $ git shortlog e78b70e59..468324a --no-merges
    Anthony Lombardi (1):
          BUG: Fix edge cases in Slerp
    ```

--- SuperBuild/External_Autoscoper.cmake
+++ SuperBuild/External_Autoscoper.cmake
@@ -35,7 +35,7 @@ if(NOT DEFINED ${proj}_DIR AND NOT ${SUPERBUILD_TOPLEVEL_PROJECT}_USE_SYSTEM_${p
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "e78b70e594b8eda8e6b92a1d91c2eb8732d1766d"
+    "468324a92840b62653c52474ff20bc7897284bd0"
     QUIET
   )
 
